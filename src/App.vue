e0cade38387d45dfd3c55ea0b4b44929c194bd25
Yorjandy <yorjandy5@gmail.com>
2024-08-12 20:10:49 +0200

    geosphere 1.0.2 : Cambiando estructura base del proyecto, cambio de libreria para testing se quito mocha y se agrego vitest para poder realizar las pruebas unitarias en vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Map } from 'leaflet';
+import { onMounted, ref } from 'vue';
+
+onMounted(()=>{
+   const map = new Map("map");
+})
+</script>
+
+<template>
+  <main>
+    <h1>Plantilla de Geosphere lista para el trabajo</h1>
+    <h4>Librerias y dependencias a usar </h4>
+    <h5>leaflet</h5>
+    <p>trabajo con informacion geografica</p>
+    <h5>vitest</h5>
+    <p> unit testing para funcionalidades y componentes</p>
+    <div id="map"></div>
+  </main>
+</template>
+
+<style>
+  #map{
+    height: 300px;
+    width: 300px;
+  }
+</style>
