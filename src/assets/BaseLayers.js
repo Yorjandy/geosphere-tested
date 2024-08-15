9900d881b45a9fc2aa1c8c7124df083ddb24b56a
dany-uclv <angiegal6969@gmail.com>
2024-08-14 19:59:13 -0500

    Geosphere version 1.0.3: Se agregaron BaseLayer.js, OverLays.js y archivos relacionados. Incluye fuentes e iconos

---
+++ src/assets/BaseLayers.js
@@ -0,0 +1,32 @@
+export const BASE = [
+    {
+        id : "default",
+        url :" http://ows.mundialis.de/services/service?",
+        name : "OSM-WMS",
+        format: "image/png"
+    },
+    {
+        id :" topografia",
+        url : "http://ows.mundialis.de/services/service?",
+        name: "TOPO-WMS",
+        format: "image/png"
+    },
+    {
+        id : "contorno",
+        url : "http://ows.mundialis.de/services/service?",
+        name: "SRTM30-Contour",
+        format: "image/png"
+    },
+    {
+        id : "oscuro",
+        url : "http://ows.mundialis.de/services/service?",
+        name: "Dark",
+        format: "image/png"
+    },
+    {
+        id : "carreteras",
+        url : "http://ows.mundialis.de/services/service?",
+        name: "OSM-Overlay-WMS",
+        format: "image/png"
+    }
+];
---
+++ src/assets/OverLays.js
@@ -0,0 +1,16 @@
+export const OVER =[
+    {
+        id : "Municipios",
+        url : "http://localhost:8080/geoserver/viteGeo/wms",
+        name : "viteGeo:municipios",
+        format : "image/png",
+        srs : "EPSG%3A4326"
+    },
+    {
+        id: "Provincias",
+        url : "http://localhost:8080/geoserver/viteGeo/wms",
+        name : "viteGeo:provincias",
+        format : "image/png",
+        srs : "EPSG%3A4326"    
+    }
+];
